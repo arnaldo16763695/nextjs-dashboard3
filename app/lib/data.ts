0e40e98a15d441a9282f59fdf3d9d861157e8c41
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-07-07 14:17:49 -0400

    added streaming and optimizations

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,7 +16,6 @@ export async function fetchRevenue() {
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
@@ -30,6 +29,12 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+  
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
